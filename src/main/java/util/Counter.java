8c6088af0708880a5d7c4d23ced1ba501d20d438
NZ_NTDPZZ <97873968+Ntdpz@users.noreply.github.com>
2024-01-19 08:09:20 +0000

    Exercise 2 DEV1

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -29,14 +29,18 @@ public class Counter {
 	}
 
 	// TODO: dev1- method for increment to closest even number
-	public void incrementToEven() {
-		_ctr = -99;
-	}
+public void incrementToEven() {
+    if (_ctr % 2 != 0) {
+        _ctr++; // Increment if the current value is odd to make it even
+    }
+}
 
-	// TODO: dev1- method for decrement to closest even number
-	public void decrementToEven() {
-		_ctr = -99;
-	}
+// TODO: dev1- method for decrement to closest even number
+public void decrementToEven() {
+    if (_ctr % 2 != 0) {
+        _ctr--; // Decrement if the current value is odd to make it even
+    }
+}
 
 	// TODO: dev2- method for increment to closest prime number
 	public void incrementToPrime() {
